cc985815df8717b6a761d8a99fcecedc409f492a
Mateusz <kudej@hotelnetsolutions.de>
2021-02-13 18:03:02 +0100

    Fixed mistake in property interface

--- src/interfaces/Property.ts
+++ src/interfaces/Property.ts
@@ -1,13 +1,11 @@
-import { StyledInterface } from "styled-components";
-
 export interface Property {
-    city: string;
-    country: string;
-    superHost: boolean;
-    title: string;
-    rating: number;
-    maxGuests: number;
-    type: string;
-    beds: number;
-    photo: string;
+  city: string;
+  country: string;
+  superHost: boolean;
+  title: string;
+  rating: number;
+  maxGuests: number;
+  type: string;
+  beds: number;
+  photo: string;
 }
