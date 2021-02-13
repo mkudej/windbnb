58ea1480fbcabd8186139ad43e8498fe4bc07f7b
Mateusz <kudej@hotelnetsolutions.de>
2021-02-13 18:07:32 +0100

    Using ssot in guests filter

--- src/components/Header/HeaderSearcher/GuestsFilter/GuestsFilter.tsx
+++ src/components/Header/HeaderSearcher/GuestsFilter/GuestsFilter.tsx
@@ -1,19 +1,39 @@
+import { Guests } from "../../../../interfaces/Guests";
 import IntegerInput from "./IntegerInput/IntegerInput";
 import { FilterWrapper, StyledGuestsFilter } from "./styles";
 
-const GuestsFilter = () => (
-  <StyledGuestsFilter>
-    <FilterWrapper>
-      <span>Adults</span>
-      <span className="description">Ages 13 or above</span>
-      <IntegerInput max={10} />
-    </FilterWrapper>
-    <FilterWrapper>
-      <span>Children</span>
-      <span className="description">Ages 2-12</span>
-      <IntegerInput max={10} />
-    </FilterWrapper>
-  </StyledGuestsFilter>
-);
+interface GuestsFilterProps {
+  guests: Guests;
+  setGuests: any;
+}
+
+const GuestsFilter = ({ guests, setGuests }: GuestsFilterProps) => {
+  const changeAdults = (value: number) => {
+    setGuests({ ...guests, adults: value, total: value + guests.children });
+  };
+
+  const changeChildren = (value: number) => {
+    setGuests({ ...guests, children: value, total: value + guests.adults });
+  };
+
+  return (
+    <StyledGuestsFilter>
+      <FilterWrapper>
+        <span>Adults</span>
+        <span className="description">Ages 13 or above</span>
+        <IntegerInput value={guests.adults} onChange={changeAdults} max={10} />
+      </FilterWrapper>
+      <FilterWrapper>
+        <span>Children</span>
+        <span className="description">Ages 2-12</span>
+        <IntegerInput
+          value={guests.children}
+          onChange={changeChildren}
+          max={10}
+        />
+      </FilterWrapper>
+    </StyledGuestsFilter>
+  );
+};
 
 export default GuestsFilter;
