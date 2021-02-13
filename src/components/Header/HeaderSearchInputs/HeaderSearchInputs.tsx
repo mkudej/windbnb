e356be134f48f7da9a3fda6cfb6e8343682e7ea6
Mateusz <kudej@hotelnetsolutions.de>
2021-02-13 18:32:33 +0100

    Getting values in search preview

--- src/components/Header/HeaderSearchInputs/HeaderSearchInputs.tsx
+++ src/components/Header/HeaderSearchInputs/HeaderSearchInputs.tsx
@@ -1,14 +1,25 @@
 import SearchIcon from "@material-ui/icons/Search";
+import { useContext } from "react";
+import FiltersContext from "../../../context/Filters";
+import GuestsTextValue from "../HeaderSearcher/GuestsTextValue/GuestsTextValue";
+import LocationTextValue from "../HeaderSearcher/LocationTextValue/LocationTextValue";
 import { StyledHeaderSearchInputs, StyledHeaderSearchButton } from "./styles";
 
-const HeaderSearchInputs = () => (
-  <StyledHeaderSearchInputs>
-    <StyledHeaderSearchButton>Add Location</StyledHeaderSearchButton>
-    <StyledHeaderSearchButton>Add Guests</StyledHeaderSearchButton>
-    <StyledHeaderSearchButton>
-      <SearchIcon style={{ color: "#EB5757" }} />
-    </StyledHeaderSearchButton>
-  </StyledHeaderSearchInputs>
-);
+const HeaderSearchInputs = () => {
+  const { location, guests } = useContext(FiltersContext);
+  return (
+    <StyledHeaderSearchInputs>
+      <StyledHeaderSearchButton>
+        <LocationTextValue value={location} />
+      </StyledHeaderSearchButton>
+      <StyledHeaderSearchButton>
+        <GuestsTextValue value={guests.total} />
+      </StyledHeaderSearchButton>
+      <StyledHeaderSearchButton>
+        <SearchIcon style={{ color: "#EB5757" }} />
+      </StyledHeaderSearchButton>
+    </StyledHeaderSearchInputs>
+  );
+};
 
 export default HeaderSearchInputs;
