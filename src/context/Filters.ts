c542f74504446fe1222aad67512002c642cfd82b
Mateusz <kudej@hotelnetsolutions.de>
2021-02-13 18:04:11 +0100

    Fixed filters context to use new interface

--- src/context/Filters.ts
+++ src/context/Filters.ts
@@ -1,12 +1,12 @@
 import { createContext } from "react";
-
-interface Filters {
-  guests: number;
-  location: string;
-}
+import { Filters } from "../interfaces/Filters";
 
 const FiltersContext = createContext<Filters>({
-  guests: 0,
+  guests: {
+    adults: 0,
+    children: 0,
+    total: 0,
+  },
   location: "",
 });
 
